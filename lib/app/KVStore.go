6935cacde259f6bcfcbfce78b6264cbd4435634c
0xzre <alilo.ghazali@gmail.com>
2024-06-08 22:29:31 +0700

    implement kv service

--- lib/app/KVStore.go
+++ lib/app/KVStore.go
@@ -1,5 +1,7 @@
 package app
 
+import "errors"
+
 type KVStore struct {
 	store map[string]string
 }
@@ -14,36 +16,45 @@ func (kv *KVStore) Ping() string {
 	return "PONG"
 }
 
-func (kv *KVStore) Get(key string) string {
+func (kv *KVStore) Get(key string) (string, error) {
 	value, ok := kv.store[key]
 	if !ok {
-		return ""
+		return "", errors.New("key not found")
 	}
-	return value
+	return value, nil
 }
 
-func (kv *KVStore) Set(key, value string) string {
+func (kv *KVStore) Set(key, value string) (string, error) {
+	if key == "" {
+		return "ERROR", errors.New("key cannot be empty")
+	}
 	kv.store[key] = value
-	return "OK"
+	return "OK", nil
 }
 
-func (kv *KVStore) Strlen(key string) int {
-	value := kv.Get(key)
-	return len(value)
+func (kv *KVStore) Strlen(key string) (int, error) {
+	value, err := kv.Get(key)
+	if err != nil {
+		return 0, err
+	}
+	return len(value), nil
 }
 
-func (kv *KVStore) Delete(key string) string {
+func (kv *KVStore) Delete(key string) (string, error) {
 	value, ok := kv.store[key]
 	if !ok {
-		return ""
+		return "", errors.New("key not found")
 	}
 	delete(kv.store, key)
-	return value
+	return value, nil
 }
 
-func (kv *KVStore) Append(key, value string) string {
-	existing := kv.Get(key)
+func (kv *KVStore) Append(key, value string) (string, error) {
+	existing, err := kv.Get(key)
+	if err != nil {
+		return "", err
+	}
 	new_value := existing + value
 	kv.store[key] = new_value
-	return "OK"
+	return "OK", nil
 }
