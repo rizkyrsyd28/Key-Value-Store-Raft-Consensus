1fa6b94b4447b7a6a6c748de342ecc3f8c9f691f
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2024-06-09 16:56:56 +0700

    feat: init web client

---
+++ web/templates/main.js
@@ -0,0 +1,34 @@
+// Network
+const ipInput = document.querySelector("#ip");
+const portInput = document.querySelector("#port");
+const btnAddress = document.querySelector("#btn-address");
+const resAddress = document.querySelector("#res-address");
+
+// Gat
+const getKeyInput = document.querySelector("#get-key");
+const btnGet = document.querySelector("#btn-get");
+const resGet = document.querySelector("#res-get");
+
+// Set
+const setKeyInput = document.querySelector("#set-key");
+const setValInput = document.querySelector("#set-val");
+const btnSet = document.querySelector("#btn-set");
+const resSet = document.querySelector("#res-set");
+
+// StrLn
+const strlnKeyInput = document.querySelector("#strln-key");
+const btnStrln = document.querySelector("#btn-strln");
+const resStrln = document.querySelector("#res-strln");
+
+// Append
+const appendKeyInput = document.querySelector("#append-key");
+const appendValInput = document.querySelector("#append-val");
+const btnAppend = document.querySelector("#btn-append");
+const resAppend = document.querySelector("#res-append");
+
+// Del
+const delKeyInput = document.querySelector("#del-key");
+const btnDel = document.querySelector("#btn-del");
+const resDel = document.querySelector("#res-del");
+
+
