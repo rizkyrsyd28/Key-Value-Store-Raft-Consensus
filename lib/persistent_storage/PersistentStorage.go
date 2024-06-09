d6f61be7bc0cfc253534b9aa7bee1020ea52d199
0xzre <alilo.ghazali@gmail.com>
2024-06-09 23:47:03 +0700

    fix : persistent folder

--- lib/persistent_storage/PersistentStorage.go
+++ lib/persistent_storage/PersistentStorage.go
@@ -26,6 +26,10 @@ type PersistentStorage struct {
 func NewPersistentStorage(addr *util.Address) *PersistentStorage {
 	id := fmt.Sprintf("%s_%d", addr.IP, addr.Port)
 	path := fmt.Sprintf("persistent/%s.json", id)
+	// make folder if not exist
+	if _, err := os.Stat("persistent"); os.IsNotExist(err) {
+		os.Mkdir("persistent", 0755)
+	}
 
 	return &PersistentStorage{
 		ID:   id,
